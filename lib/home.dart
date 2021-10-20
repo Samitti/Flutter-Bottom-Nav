64cf95214b333913d270548ceec225a2d295b7fd
Samitti <samuelteweldebrhan@gmail.com>
2021-10-20 11:59:36 +0300

    add dialog alert

--- lib/home.dart
+++ lib/home.dart
@@ -1,34 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  double boxHight = 300;
-  double boxWidth = 300;
-
-  void _shrinkBox () {
-    setState(() {
-      boxHight = 100;
-      boxWidth = 100;
-    });
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('Title'),
+            content: Text('Contetbt, more info'),
+            actions: [
+              MaterialButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: Text('Button'),
+              )
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _shrinkBox,
-      child: Scaffold(
-        body: Center(
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            height: boxHight,
-            width: boxWidth,
-            color: Colors.amber,
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Center(
+        child: MaterialButton(
+          color: Colors.deepPurple[600],
+          onPressed: _showDialog,
+          child: Text(
+            'Show DIALOG',
+            style: TextStyle(fontSize: 30),
           ),
         ),
       ),
